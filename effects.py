1c40224336356a660f381dcc9ddb01b8183608d6
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2020-11-14 13:05:02 -0800

    testing code out

--- effects.py
+++ effects.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def get_ball(image):
     radius = 20
@@ -7,3 +8,14 @@ def get_ball(image):
     center_coordinates = (50, 50)
     new_image = cv2.circle(image, center_coordinates, radius, color, thickness)
     return new_image
+
+def colourize_image(image):
+    hsvim = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower = np.array([0, 48, 80], dtype="uint8")
+    upper = np.array([20, 255, 255], dtype="uint8")
+    skinRegionHSV = cv2.inRange(hsvim, lower, upper)
+    blurred = cv2.blur(skinRegionHSV, (2, 2))
+    ret, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY)
+    return thresh
+    #cv2.imshow("thresh", thresh)
+    #print('test')
